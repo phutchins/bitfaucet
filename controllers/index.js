b00d4711b931854f61d8d398bb71d8baea0511a3
Philip Hutchins <flipture@gmail.com>
2014-10-04 15:57:18 -0400

    got balance working, still need to get rid of surround

--- controllers/index.js
+++ controllers/index.js
@@ -11,9 +11,9 @@ module.exports.controller = function(app) {
       if (err) return console.log(err);
       console.log('Balance:', btcBalance);
       btcBalance = parseFloat(balance);
-    });
-    Pour.find( function(err, pours) {
-      res.render('index', {pours: pours, balance: btcBalance, pourmessage: "no message"});
+      Pour.find( function(err, pours) {
+        res.render('index', {pours: pours, balance: btcBalance, pourmessage: "no message"});
+      });
     });
   });
 
@@ -22,18 +22,18 @@ module.exports.controller = function(app) {
     console.log("User with IP " + req.connection.remoteAddress + " has poured " + req.body.pour_ammount + " to address " + req.body.wallet_address);
     btcClient.sendToAddress(req.body.wallet_address, parseFloat(req.body.pour_ammount), function(err, txid) {
       if (err) return console.error(err);
-    });
-    var pour = new Pour({
-      ip: req.connection.remoteAddress,
-      pour_ammount: req.body.pour_ammount,
-      comment: req.param.comment
-    });
-    pour.save(function(err, item) {
-      if (err) return console.error(err);
-      //console.dir(item);
-      Pour.find( function(err, pours) {
-        pourMessage = 'Successfully poured ' + req.body.pour_ammount + ' to ' + req.body.wallet_address;
-        res.render('index', {pours: pours, balance: btcBalance, pourmessage: pourMessage});
+      var pour = new Pour({
+        ip: req.connection.remoteAddress,
+        pour_ammount: req.body.pour_ammount,
+        comment: req.param.comment
+      });
+      pour.save(function(err, item) {
+        if (err) return console.error(err);
+        //console.dir(item);
+        Pour.find( function(err, pours) {
+          pourMessage = 'Successfully poured ' + req.body.pour_ammount + ' to ' + req.body.wallet_address;
+          res.render('index', {pours: pours, balance: btcBalance, pourmessage: pourMessage});
+        });
       });
     });
   });
