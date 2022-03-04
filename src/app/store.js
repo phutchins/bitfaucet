fdfa0ceb8e84fe01184b778e145f0200994c631d
victorwu <victorwu@users.noreply.github.com>
2022-03-03 18:12:18 -0800

    style: semicolon and tabs fixes

--- src/app/store.js
+++ src/app/store.js
@@ -2,5 +2,6 @@ import { configureStore } from '@reduxjs/toolkit'
 import faucetReducer from '../features/faucet/faucetSlice'
 
 export const store = configureStore({
-  reducer: faucetReducer,
-})
+    reducer: faucetReducer,
+  }
+);
