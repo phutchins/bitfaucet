39934d7fce824e260affc8a2fa64339638808def
victorwu <victorwu@users.noreply.github.com>
2022-03-07 12:32:00 -0800

    revert: FaucetDripForm to class component from 06572678daf9b791b5981333980d76d1c35fd4b1

--- src/components/FaucetDripForm.js
+++ src/components/FaucetDripForm.js
@@ -6,16 +6,9 @@ import * as bitcoin from 'bitcoinjs-lib';
 
 // React
 import merge from 'lodash.merge';
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { 
-  recipientAddressClear, 
-  recipientAddressUpdate,
-  setState,
-  statusUpdate,
-} from '../features/faucet/faucetSlice'
+import React from 'react';
+// import { connect } from 'react-redux';
 // import FabricStateMapper from '../StateMapper';
-import { store } from '../app/store';
 
 // Fabric Types
 import FabricComponent from '../types/component';
@@ -38,44 +31,44 @@ import {
 
 // Fabric Types
 import * as Actor from '@fabric/core/types/actor';
-import { render } from 'react-dom';
 // import * as Key from '@fabric/core/types/key';
 // import * as Signer from '@fabric/core/types/signer';
 
+class FaucetDripForm extends FabricComponent {
+  constructor (props) {
+    super(props);
 
-export default function FaucetDripForm (props) {
-  const [inputAddress, setInputAddress] = useState(useSelector((state) => state.recipient));
-  const dispatch = useDispatch();
-
-  const settings = Object.assign({
-    network: 'regtest'
-  }, props);
+    this.settings = Object.assign({
+      network: 'regtest'
+    }, props);
 
     /* this.wallet = new Wallet({
-    network: this.settings.network
-  }); */
+      network: this.settings.network
+    }); */
 
-  // this.key = new Key(this.settings);
-  // this._key = new bcoin.hd.key();
+    // this.key = new Key(this.settings);
+    // this._key = new bcoin.hd.key();
 
-  // TODO: prepare Fabric
-  // i.e., use _state here, then import from getter and apply properties
-  // _from_ @react
-  const state = useSelector((state) => Object.assign({
-    address: inputAddress,
-    content: {
-      requests: []
-    },
-    requests: {},
-    secret: Actor.randomBytes(32), // solution hash (revealed on trade)
-    status: 'LOADING'
-  }, props, state));
-  
-  // TODO: evaluate removing ZMQ
-  // this.bitcoin = new Bitcoin(this.settings);
+    // TODO: prepare Fabric
+    // i.e., use _state here, then import from getter and apply properties
+    // _from_ @react
+    this.state = merge({
+      address: null,
+      content: {
+        requests: []
+      },
+      requests: {},
+      secret: Actor.randomBytes(32), // solution hash (revealed on trade)
+      status: 'LOADING'
+    }, props);
 
+    // TODO: evaluate removing ZMQ
+    // this.bitcoin = new Bitcoin(this.settings);
 
-  const networks = () => {
+    return this;
+  }
+
+  get networks () {
     return {
       'mainnet': bitcoin.networks.mainnet,
       'regtest': bitcoin.networks.regtest,
@@ -83,7 +76,7 @@ export default function FaucetDripForm (props) {
     };
   }
 
-  const _handleAddressChange = (e) => {
+  _handleAddressChange (e) {
     this.setState({ address: e.target.value });
     const isValid = this.validateAddress(e.target.value);
     if (!isValid) {
@@ -96,39 +89,64 @@ export default function FaucetDripForm (props) {
     }
   }
 
-  const _handleSecretChange = (e) => {
+  _handleSecretChange (e) {
     this.setState({ secret: e.target.value });
   }
 
-  const _handleSubmitButtonClick = (e) => {
+  _handleSubmitButtonClick (e) {
     console.log('submit button click:', e);
   }
 
-  const _handleChange = (e) => {
+  _handleChange (e) {
     const fields = { ...this.state.fields, [e.target.name]: e.target.value };
     // TODO: merge old state
-    this._handleAddressChange(e);
     this.setState({ fields: fields });
   }
 
-  const handleChange = (e) => {
-    // setInputAddress(e.target.value);
-    // setTimeout(function () {
-  
-      // TODO: add debounce
-
-      // if(inputAddress) { // TODO: add validateAddress
-        dispatch(recipientAddressUpdate(e.target.value));
-      // } else dispatch(recipientAddressUpdate(''));
-      
-    // }, 1000);
+  handleChange (e) {
+    this.setState({ address: e.target.value });
   }
 
-  const handleSubmit = (e) => {
-    _handleSubmitButtonClick(e);
+  render () {
+    return (
+      <>
+        <Form
+          ref={this.props.form}
+          loading={(this.state.status === 'LOADING' ? 'loading' : undefined)}
+          disabled={(this.state.status === 'LOADING' ? 'disabled' : undefined)}
+          onSubmit={this.props.onSubmit.bind(this)} onChange={this.handleChange.bind(this)}>
+          <Form.Field>
+            <label>Request a deposit to&hellip;</label>
+            <div className='ui input'>
+              <Input ref={this.props.field} action type='text' placeholder='Enter a Bitcoin address here' value={this.state.address} />
+              <Button ref={this.props.button}
+                attached
+                type='submit'
+                loading={(this.state.status === 'SUBMITTING') ? 'loading' : undefined}
+                disabled={(this.state.status === 'SUBMITTING') ? 'disabled' : undefined}
+                color='green' content='Request' icon='right chevron' labelPosition='right' onClick={this.props.onSubmit.bind(this)} />
+            </div>
+            <FabricModal />
+          </Form.Field>
+        </Form>
+      </>
+    );
   }
 
-  const validateAddress = (address) => {
+  start () {
+    console.log('[FAUCET:DRIPS]', 'Starting component...');
+    super.start();
+    this.sync();
+    return this;
+  }
+
+  sync () {
+    const preimage = createHash('sha256').update(this.state.secret).digest('hex');
+    this.setState({ preimage: preimage });
+    return this;
+  }
+
+  validateAddress (address) {
     try {
       bitcoin.address.toOutputScript(address, this.networks[this.settings.network]);
       return true;
@@ -136,44 +154,7 @@ export default function FaucetDripForm (props) {
       return false;
     }
   }
-  
-  return (
-    <>
-      <Form
-        ref={props.form}
-        loading={(state.status === 'LOADING')}
-        disabled={(state.status === 'LOADING')}
-        onSubmit={props.onSubmit.bind(this)}>
-        <Form.Field>
-          <label>Request a deposit to&hellip;</label>
-          <div className='ui input'>
-            <Input ref={props.field} action type='text' placeholder='Enter a Bitcoin address here' value={inputAddress} onChange={(e) => handleChange(e)} />
-            <Button
-              attached
-              type='submit'
-              loading={(state.status === 'REQUESTING')}
-              disabled={(state.status === 'REQUESTING') || (state.status === 'PAUSED') || (state.status === 'ERROR')}
-              color='green' content='Request' icon='right chevron' labelPosition='right' onClick={props.onSubmit.bind(this)} />
-          </div>
-          <FabricModal />
-        </Form.Field>
-      </Form>
-    </>
-  )
-
-
-
-  // start () {
-  //   console.log('[FAUCET:DRIPS]', 'Starting component...');
-  //   super.start();
-  //   this.sync();
-  //   return this;
-  // }
-
-  // sync () {
-  //   const preimage = createHash('sha256').update(this.state.secret).digest('hex');
-  //   this.setState({ preimage: preimage });
-  //   return this;
-  // }
-
 }
+
+// export default PortalOrderForm;
+export default FaucetDripForm;
